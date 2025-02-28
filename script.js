d2b50bf914dce7f6e9b3b5834a88c0c1bedadc2d
Mirko Albanese <mirkoalba006@gmail.com>
2025-02-28 14:48:03 +0100

    add resize

--- script.js
+++ script.js
@@ -18,12 +18,26 @@ window.addEventListener("load", function () {
   VIEW.width = window.innerWidth;
   VIEW.height = window.innerHeight;
 
+  // Boxes default options
   var defaultObjectOptions = {
     // mass: 2,
     restitution: 0,
     friction: 1,
     // sleepThreshold: 80,
     // frictionAir: 0.025, // increasing the frictionAir will make the boxes fall slower
+  };
+
+  // Walls default options
+  var wallopts = {
+    isStatic: true,
+    restitution: 0,
+    friction: 1,
+  };
+
+  var groundopts = {
+    isStatic: true,
+    restitution: 0,
+    friction: 1,
   };
 
   // create engine
@@ -89,314 +103,336 @@ window.addEventListener("load", function () {
   Runner.run(runner, engine);
 
   // add walls
-  var wallopts = {
-    isStatic: true,
-    restitution: 0,
-    friction: 1,
-  };
 
-  var groundopts = {
-    isStatic: true,
-    restitution: 0,
-    friction: 1,
-  };
+  function createBodies() {
+    Composite.add(world, [
+      // ground
+      (ground = Bodies.rectangle(
+        VIEW.width / 2,
+        VIEW.height + 50,
+        VIEW.width + 200,
+        100,
+        groundopts
+      )),
+      // walls
+      (rightWall = Bodies.rectangle(
+        VIEW.width + 50,
+        VIEW.height / 2,
+        100,
+        VIEW.height,
+        wallopts
+      )), // right
+      (leftWall = Bodies.rectangle(
+        -50,
+        VIEW.height / 2,
+        100,
+        VIEW.height,
+        wallopts
+      )), // left
+    ]);
 
-  World.add(world, [
-    // ground
-    Bodies.rectangle(
-      VIEW.width / 2,
-      VIEW.height + 50,
-      VIEW.width + 200,
-      100,
-      groundopts
-    ),
-    // walls
-    Bodies.rectangle(
-      VIEW.width + 50,
-      VIEW.height / 2,
-      100,
-      VIEW.height,
-      wallopts
-    ), // right
-    Bodies.rectangle(-50, VIEW.height / 2, 100, VIEW.height, wallopts), // left
-  ]);
+    // Boxes
 
-  // Boxes
+    var bodies = [],
+      elements = [];
 
-  var bodies = [],
-    elements = [];
+    var blueBox = document.getElementById("blue-box"),
+      orangeBox = document.getElementById("orange-box"),
+      pinkBox = document.getElementById("pink-box");
 
-  var blueBox = document.getElementById("blue-box"),
-    orangeBox = document.getElementById("orange-box"),
-    pinkBox = document.getElementById("pink-box");
+    var blueBoxPositionInfo = blueBox.getBoundingClientRect(),
+      blueBoxWidth = blueBoxPositionInfo.width,
+      blueBoxHeight = blueBoxPositionInfo.height;
 
-  var blueBoxPositionInfo = blueBox.getBoundingClientRect(),
-    blueBoxWidth = blueBoxPositionInfo.width,
-    blueBoxHeight = blueBoxPositionInfo.height;
+    var orangeBoxPositionInfo = orangeBox.getBoundingClientRect(),
+      orangeBoxWidth = orangeBoxPositionInfo.width,
+      orangeBoxHeight = orangeBoxPositionInfo.height;
 
-  var orangeBoxPositionInfo = orangeBox.getBoundingClientRect(),
-    orangeBoxWidth = orangeBoxPositionInfo.width,
-    orangeBoxHeight = orangeBoxPositionInfo.height;
+    var pinkBoxPositionInfo = pinkBox.getBoundingClientRect(),
+      pinkBoxWidth = pinkBoxPositionInfo.width,
+      pinkBoxHeight = pinkBoxPositionInfo.height;
 
-  var pinkBoxPositionInfo = pinkBox.getBoundingClientRect(),
-    pinkBoxWidth = pinkBoxPositionInfo.width,
-    pinkBoxHeight = pinkBoxPositionInfo.height;
+    // Elements arraytorender with options
+    elements = [
+      {
+        type: "rectangle",
+        el: blueBox,
+        x: VIEW.width / 2,
+        y: VIEW.height - blueBoxHeight / 2 - orangeBoxHeight - pinkBoxHeight,
+        options: {},
+      },
+      {
+        type: "ellipse",
+        el: orangeBox,
+        x: VIEW.width / 2,
+        y: VIEW.height - orangeBoxHeight / 2 - pinkBoxHeight,
+        options: {},
+      },
+      {
+        type: "circle",
+        el: pinkBox,
+        x: VIEW.width / 2,
+        y: VIEW.height - pinkBox.offsetHeight / 2,
+        options: { isStatic: true },
+      },
+    ];
 
-  // Elements arraytorender with options
-  elements = [
-    {
-      type: "rectangle",
-      el: blueBox,
-      x: VIEW.width / 2,
-      y: VIEW.height - blueBoxHeight / 2 - orangeBoxHeight - pinkBoxHeight,
-      options: {},
-    },
-    {
-      type: "ellipse",
-      el: orangeBox,
-      x: VIEW.width / 2,
-      y: VIEW.height - orangeBoxHeight / 2 - pinkBoxHeight,
-      options: {},
-    },
-    {
-      type: "circle",
-      el: pinkBox,
-      x: VIEW.width / 2,
-      y: VIEW.height - pinkBox.offsetHeight / 2,
-      options: { isStatic: true },
-    },
-  ];
+    elements.forEach(function (element, i) {
+      var body;
 
-  elements.forEach(function (element, i) {
-    var body;
+      switch (element.type) {
+        case "circle":
+          body = Bodies.circle(
+            element.x,
+            element.y,
+            pinkBoxWidth / 2,
+            deepMerge(
+              {},
+              {
+                ...defaultObjectOptions,
+                ...element.options,
+              }
+            )
+          );
+          break;
 
-    switch (element.type) {
-      case "circle":
-        body = Bodies.circle(
-          element.x,
-          element.y,
-          pinkBoxWidth / 2,
-          deepMerge(
-            {},
-            {
-              ...defaultObjectOptions,
-              ...element.options,
-            }
-          )
-        );
-        break;
+        case "ellipse":
+          body = createEllipse(
+            element.x,
+            element.y,
+            orangeBoxWidth,
+            orangeBoxHeight,
+            deepMerge(
+              {},
+              {
+                ...defaultObjectOptions,
+                ...element.options,
+              }
+            )
+          );
+          break;
 
-      case "ellipse":
-        body = createEllipse(
-          element.x,
-          element.y,
-          orangeBoxWidth,
-          orangeBoxHeight,
-          deepMerge(
-            {},
-            {
-              ...defaultObjectOptions,
-              ...element.options,
-            }
-          )
-        );
-        break;
+        default:
+          body = Bodies.rectangle(
+            element.x,
+            element.y,
+            blueBoxWidth,
+            blueBoxHeight,
+            deepMerge(
+              {},
+              {
+                ...defaultObjectOptions,
+                ...element.options,
+              }
+            )
+          );
 
-      default:
-        body = Bodies.rectangle(
-          element.x,
-          element.y,
-          blueBoxWidth,
-          blueBoxHeight,
-          deepMerge(
-            {},
-            {
-              ...defaultObjectOptions,
-              ...element.options,
-            }
-          )
-        );
-
-        break;
-    }
-
-    body.id = element.el.id;
-
-    bodies.push(body);
-
-    element.el.style.opacity = 1; // make the element visible again
-  });
-
-  var blueBody = bodies[0];
-  var orangeBody = bodies[1];
-  var pinkBody = bodies[2];
-
-  // Link the bodies
-
-  var constraintPinkToOrange = Constraint.create({
-    bodyA: pinkBody,
-    pointA: { x: 0, y: -pinkBoxHeight / 2 }, // top of the pink box
-    bodyB: orangeBody,
-    pointB: { x: 0, y: orangeBoxHeight / 2 }, // top of the orange box
-    length: 1,
-    stiffness: 1,
-  });
-
-  var constraintBlueToOrange = Constraint.create({
-    bodyA: blueBody,
-    pointA: { x: 0, y: blueBoxHeight / 2 },
-    bodyB: orangeBody,
-    pointB: { x: 0, y: -orangeBoxHeight / 2 },
-    length: 2,
-    stiffness: 1,
-  });
-
-  // Add objects to the world
-  Composite.add(world, [
-    blueBody,
-    orangeBody,
-    pinkBody,
-    constraintPinkToOrange,
-    constraintBlueToOrange,
-  ]);
-
-  /**
-   * Animate objects
-   */
-
-  // Pink body variables
-  var pinkBodyInitialX = pinkBody.position.x,
-    pinkBodyOffset = 80,
-    pinkBodySpeed = 0.002;
-
-  // Orange body variables
-  var maxAngle = 12 * (Math.PI / 180),
-    minAngle = -12 * (Math.PI / 180),
-    rotationDirection = 1,
-    rotationSpeed = 0.003;
-
-  // Blue body variables
-  var maxAngleBlueBody = 14 * (Math.PI / 180),
-    minAngleBlueBody = -14 * (Math.PI / 180),
-    rotationDirectionBlueBody = 1,
-    rotationSpeedBlueBody = 0.003;
-
-  var lastTime = 0,
-    forceDirection = 1,
-    forceIntensity = 1.5;
-
-  Events.on(engine, "beforeUpdate", function (event) {
-    var time = engine.timing.timestamp,
-      timeScale = (event.delta || 1000 / 60) / 1000;
-
-    /**
-     * Pink Body Swing
-     */
-
-    var pinkOffset = pinkBodyOffset * Math.sin(time * pinkBodySpeed);
-
-    Body.setPosition(pinkBody, {
-      x: pinkBodyInitialX + pinkOffset,
-      y: pinkBody.position.y,
-    });
-
-    /**
-     * Orange Body Swing
-     */
-
-    // var newAngle = orangeBody.angle + rotationSpeed * rotationDirection;
-
-    // if (newAngle >= maxAngle) {
-    //   newAngle = maxAngle;
-    //   rotationDirection = -1;
-    // } else if (newAngle <= minAngle) {
-    //   newAngle = minAngle;
-    //   rotationDirection = 1;
-    // }
-
-    // Body.setAngle(orangeBody, newAngle);
-
-    // every tot sec
-    // if (engine.timing.timestamp - lastTime >= 4000) {
-    //   var forceMagnitude =
-    //     forceDirection * forceIntensity * orangeBody.mass * timeScale;
-
-    //   Body.applyForce(
-    //     orangeBody,
-    //     {
-    //       x: orangeBody.position.x + orangeBoxWidth / 2,
-    //       y: orangeBody.position.y,
-    //     },
-    //     {
-    //       x: forceMagnitude,
-    //       y: 0,
-    //     }
-    //   );
-
-    //   // update last time
-    //   lastTime = engine.timing.timestamp;
-
-    //   forceDirection *= -1;
-
-    //   console.log("every tot secs");
-    // }
-
-    /**
-     * Blue Body Swing
-     */
-    // var newAngleBlueBody =
-    //   blueBody.angle + rotationSpeedBlueBody * rotationDirectionBlueBody;
-
-    // if (newAngleBlueBody >= maxAngleBlueBody) {
-    //   newAngleBlueBody = maxAngleBlueBody;
-    //   rotationDirectionBlueBody = -1;
-    // } else if (newAngleBlueBody <= minAngleBlueBody) {
-    //   newAngleBlueBody = minAngleBlueBody;
-    //   rotationDirectionBlueBody = 1;
-    // }
-
-    // Body.setAngle(blueBody, newAngleBlueBody);
-  });
-
-  Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: VIEW.width, y: VIEW.height },
-  });
-
-  if (debug) {
-    blueBox.style.opacity = 0;
-    orangeBox.style.opacity = 0;
-    pinkBox.style.opacity = 0;
-  }
-
-  if (!debug) {
-    (function update() {
-      for (var i = 0, l = elements.length; i < l; i++) {
-        var bodyDom = elements[i].el,
-          body = null;
-
-        for (var j = 0, k = bodies.length; j < k; j++) {
-          if (bodies[j].id == bodyDom.id) {
-            body = bodies[j];
-
-            break;
-          }
-        }
-
-        if (body === null) continue;
-
-        bodyDom.style.transform =
-          "translate( " +
-          (body.position.x - bodyDom.offsetWidth / 2) +
-          "px, " +
-          (body.position.y - bodyDom.offsetHeight / 2) +
-          "px )";
-        bodyDom.style.transform += "rotate( " + body.angle + "rad )";
+          break;
       }
 
-      window.requestAnimationFrame(update);
-    })();
+      body.id = element.el.id;
+
+      bodies.push(body);
+
+      element.el.style.opacity = 1; // make the element visible again
+    });
+
+    var blueBody = bodies[0];
+    var orangeBody = bodies[1];
+    var pinkBody = bodies[2];
+
+    // Link the bodies
+
+    var constraintPinkToOrange = Constraint.create({
+      bodyA: pinkBody,
+      pointA: { x: 0, y: -pinkBoxHeight / 2 }, // top of the pink box
+      bodyB: orangeBody,
+      pointB: { x: 0, y: orangeBoxHeight / 2 }, // top of the orange box
+      length: 1,
+      stiffness: 1,
+    });
+
+    var constraintBlueToOrange = Constraint.create({
+      bodyA: blueBody,
+      pointA: { x: 0, y: blueBoxHeight / 2 },
+      bodyB: orangeBody,
+      pointB: { x: 0, y: -orangeBoxHeight / 2 },
+      length: 2,
+      stiffness: 1,
+    });
+
+    // Add objects to the world
+    Composite.add(world, [
+      blueBody,
+      orangeBody,
+      pinkBody,
+      constraintPinkToOrange,
+      constraintBlueToOrange,
+    ]);
+
+    /**
+     * Animate objects
+     */
+
+    // Pink body variables
+    var pinkBodyInitialX = pinkBody.position.x,
+      pinkBodyOffset = 80,
+      pinkBodySpeed = 0.002;
+
+    // Orange body variables
+    var maxAngle = 12 * (Math.PI / 180),
+      minAngle = -12 * (Math.PI / 180),
+      rotationDirection = 1,
+      rotationSpeed = 0.003;
+
+    // Blue body variables
+    var maxAngleBlueBody = 14 * (Math.PI / 180),
+      minAngleBlueBody = -14 * (Math.PI / 180),
+      rotationDirectionBlueBody = 1,
+      rotationSpeedBlueBody = 0.003;
+
+    var lastTime = 0,
+      forceDirection = 1,
+      forceIntensity = 1.5;
+
+    Events.on(engine, "beforeUpdate", function (event) {
+      var time = engine.timing.timestamp,
+        timeScale = (event.delta || 1000 / 60) / 1000;
+
+      /**
+       * Pink Body Swing
+       */
+
+      var pinkOffset = pinkBodyOffset * Math.sin(time * pinkBodySpeed);
+
+      Body.setPosition(pinkBody, {
+        x: pinkBodyInitialX + pinkOffset,
+        y: pinkBody.position.y,
+      });
+
+      /**
+       * Orange Body Swing
+       */
+
+      // var newAngle = orangeBody.angle + rotationSpeed * rotationDirection;
+
+      // if (newAngle >= maxAngle) {
+      //   newAngle = maxAngle;
+      //   rotationDirection = -1;
+      // } else if (newAngle <= minAngle) {
+      //   newAngle = minAngle;
+      //   rotationDirection = 1;
+      // }
+
+      // Body.setAngle(orangeBody, newAngle);
+
+      // every tot sec
+      // if (engine.timing.timestamp - lastTime >= 4000) {
+      //   var forceMagnitude =
+      //     forceDirection * forceIntensity * orangeBody.mass * timeScale;
+
+      //   Body.applyForce(
+      //     orangeBody,
+      //     {
+      //       x: orangeBody.position.x + orangeBoxWidth / 2,
+      //       y: orangeBody.position.y,
+      //     },
+      //     {
+      //       x: forceMagnitude,
+      //       y: 0,
+      //     }
+      //   );
+
+      //   // update last time
+      //   lastTime = engine.timing.timestamp;
+
+      //   forceDirection *= -1;
+
+      //   console.log("every tot secs");
+      // }
+
+      /**
+       * Blue Body Swing
+       */
+      // var newAngleBlueBody =
+      //   blueBody.angle + rotationSpeedBlueBody * rotationDirectionBlueBody;
+
+      // if (newAngleBlueBody >= maxAngleBlueBody) {
+      //   newAngleBlueBody = maxAngleBlueBody;
+      //   rotationDirectionBlueBody = -1;
+      // } else if (newAngleBlueBody <= minAngleBlueBody) {
+      //   newAngleBlueBody = minAngleBlueBody;
+      //   rotationDirectionBlueBody = 1;
+      // }
+
+      // Body.setAngle(blueBody, newAngleBlueBody);
+    });
+
+    Render.lookAt(render, {
+      min: { x: 0, y: 0 },
+      max: { x: VIEW.width, y: VIEW.height },
+    });
+
+    if (debug) {
+      blueBox.style.opacity = 0;
+      orangeBox.style.opacity = 0;
+      pinkBox.style.opacity = 0;
+    }
+
+    if (!debug) {
+      (function update() {
+        for (var i = 0, l = elements.length; i < l; i++) {
+          var bodyDom = elements[i].el,
+            body = null;
+
+          for (var j = 0, k = bodies.length; j < k; j++) {
+            if (bodies[j].id == bodyDom.id) {
+              body = bodies[j];
+
+              break;
+            }
+          }
+
+          if (body === null) continue;
+
+          bodyDom.style.transform =
+            "translate( " +
+            (body.position.x - bodyDom.offsetWidth / 2) +
+            "px, " +
+            (body.position.y - bodyDom.offsetHeight / 2) +
+            "px )";
+          bodyDom.style.transform += "rotate( " + body.angle + "rad )";
+        }
+
+        window.requestAnimationFrame(update);
+      })();
+    }
   }
+
+  createBodies();
+
+  /**
+   * Handle resize
+   */
+  this.window.addEventListener("resize", function () {
+    VIEW.width = window.innerWidth;
+    VIEW.height = window.innerHeight;
+
+    render.bounds.max.x = window.innerWidth;
+    render.bounds.max.y = window.innerHeight;
+    render.options.width = window.innerWidth;
+    render.options.height = window.innerHeight;
+    render.canvas.width = window.innerWidth;
+    render.canvas.height = window.innerHeight;
+
+    Render.setPixelRatio(render, window.devicePixelRatio); // added this
+
+    // Delete all bodies
+    World.clear(engine.world, false);
+
+    // Recreate bodies
+    createBodies();
+  });
 
   function createEllipse(x, y, width, height, options, sides = 100) {
     var vertices = [];
